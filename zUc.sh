10d4eea9e0260116f8989c1c00d4bad0e4291a67
JohnGavr <47471932+JohnGavr@users.noreply.github.com>
2019-03-21 16:22:18 +0200

    Add files via upload

--- zUc.sh
+++ zUc.sh
@@ -1,53 +1,78 @@
 #!/bin/bash
 
+# Αρχικό μενού προγράμματος
 
-# Έλεγχος αν υπάρχει δίσκος USB και αν υπάρχει συνεχίζεται η διαδικασία#
-########################################################################
+file=$(zenity --list --title "Επιλογές" --text="Διαλέξτε μια από τις επιλογές" --column="Επιλέξτε" "Usb Writter" --ok-label="Quit" --cancel-label="About" --width=350 --height=350)
+
+find_disk () {
+#Διαδικασία εύρεσης δίσκου εγγραφής. 
 
 if [[ -z $(lsblk | grep media) ]];
 	then
-	echo "Δεν υπάρχει δίσκος USB"
-	echo "Τερματισμός προγράμματος"
+	zenity --error --title="Σφάλμα" --text="Δεν υπάρχει δίσκος εγγραφής" --width=350
 	exit 0
 	else
-	echo "Δίσκος USB	Μέγεθος"
-	lsblk | grep media -B1 | awk '{print $1"\t\t "$4}'
+	diskvar=$(zenity --list --title="Επιλογή Δίσκου" --column="Disk" --column="Space" \ $(lsblk -do name,tran,size | awk '$2=="usb"{print $1"\t\t"$3}')) #επιλογή και αποθήκευση δίσκου εγγραφής
 fi
-while [ "$finished" != "true" ]
-do
-	read -rp "Σε ποιο δίσκο θέλεις να κάνεις την εγκατάσταση; " diskvar;
-	if [ -z "$diskvar" ]; then
-		echo "Δεν έχεις δώσει δίσκο"
-	elif [[ $diskvar != *"/dev/sd"[a-z]* ]]; then									# Έλεγχος αν το string
-		echo "η απάντηση πρέπει να είναι της μορφης /dev/sdx" 								# περιέχει το /dev/sd
-	elif [[ ${diskvar:(-3)} != $(lsblk -m | grep -i "${diskvar:(-3)}[^0-9]" | awk '{print $1}') ]]; then		
-		echo "μη έγκυρη ονομασία δίσκου"	
-	else
-      		finished=true	
-	fi
-done
 
-########################################################################
+# Έλεγχος αν η μεταβλητή diskvar πήρε τιμή. Σε περίπτωση επιλογής cancel ή σκέτου ok χωρίς επιλογή δίσκου τερματίζει.
 
-echo "Συνέχεια διαδιακασίας..."
+if [[ -z $diskvar ]] 
+     then
+     zenity --error --title="ΣΦάλμα" --text="Δεν δηλώθηκε δίσκος εγγραφής."
+     exit 0
+     elif zenity --question --title="Προειδοποίηση" --text="Με αυτήν την ενέργεια θα χαθούν τα αρχεία στο δίσκο που επιλέξατε. Θέλετε να συνεχίσετε;" --width=350  #ερώτηση για την συνέχεια του προγράμματος
+     then 
+     sure=1   #αν η απάντηση είναι ναι τότε sure=1
+     else 
+     sure=0   #αν η απάντηση είναι όχι τότε sure=0
+fi
+
+if [[ sure -ne 1 ]] 
+     then
+     # zenity --info --title="Ενημέρωση" --text="Συνεχίζουμε"   #συνέχισε
+     
+     # else 
+     zenity --info --title="Ενημέρωση" --text="Επιλέξατε να μην γίνει η εγγραφή. Καλή συνέχεια" --width=350
+     exit 0 #σταμάτα ή επέστρεψε πίσω
+fi
+
+diskvar=$(echo $diskvar | tr -d '[:space:]') 
+}
+
+write_usb () {
+# Επιλογή αρχείου για την εγγραφή στο δίσκο που επιλέχτηκε
 
 FILE=$(zenity --file-selection --title="Επιλέξτε αρχείο" --file-filter="*.iso")
 
-echo $diskvar $FILE
+zenity --info --title="Επιλογή" --text=" Θα γίνει εγγραφή του $FILE στο $diskvar" --width=350
 
 if [[ -z $FILE ]];
 	then
-	echo "Δεν έχετε επιλέξει αρχείο...τερματισμός"
+	zenity --error -title="Σφάλμα" --text="Δεν έχετε επιλέξει αρχείο...τερματισμός"
 	exit 0
 	else
-
-
-(
-  sleep 5
-  echo "Προετοιμασία εγκατάστασης"
-  sleep 1	
-  echo "Εγγραφή USB"
-  gksudo dd if=$FILE of=$diskvar bs=1MB
-) | zenity --title="zenity USB creator" --width=350 --progress --pulsate --auto-close
-
+	(pkexec sudo dd if=$FILE of=/dev/"$diskvar" bs=1MB)| zenity --title="zenity USB creator" --width=350 --progress --pulsate 
 fi 
+}
+
+if [[ $? = "1" ]]; then # $? γίνεται ένα μόνο στην περίπτωση που πατηθεί το κουμπί About
+  
+# Menu About
+zenity --info --title="About" --text="GNU GENERAL PUBLIC LICENSE
+ Version 3, 29 June 2007
+Ένα πρόγραμμα που επιτρέπει την εγγραφή .iso αρχείων στον δίσκο της επιλογής σας." --width=350 --height=350
+
+# Επιλογή Quit τερματίζει 
+
+elif [[ $? = "0" ]] || [[ -z $file ]]; then # $? παραμένει 0 στο Quit και αν εισαχθεί και κάποια επιλογή άρα ελέγχουμε αν είναι μηδέν και η μεταβλητή $file να μην έχει πάρει τιμή
+  exit 0
+
+#Επιλογή Usb Writter
+
+elif [[ $file = "Usb Writter" ]]; then
+  find_disk
+  write_usb
+fi
+
+
